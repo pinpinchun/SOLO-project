7978fd34f3697b4e7e6a5a93695570ab73774e24
pinpinchun <pinchun0310@gmail.com>
2024-04-27 20:35:36 +0800

    search.html  search function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function doFirst(){
     
     // search 頁面
-    document.querySelector(`.list_clear`).addEventListener('click',categorylist)
+    document.querySelector(`.search_bar input`).addEventListener('keydown',enter)
+
 }
 
 
@@ -10,21 +11,12 @@ function doFirst(){
 
 // JS
 
-// 清除按鈕測試
-function categorylist(){
+// 點擊 enter 搜尋商品
+function enter(e){
 
-    let clearBtn = document.querySelector(`.list_clear`)
-    let clickedBtn = document.querySelector(`.list_btn span`).css()
-
-    clearBtn.addEventListener('click',function(){
-        
-        if(clickedBtn == 'hide'){
-
-        }else{
-
-        }
-
-    })
+    if(e.keyCode === 13){          // Enter 的按鍵碼是 13
+        document.querySelector(`.search_bar button`).click()
+    }
 
 }
 
@@ -75,7 +67,6 @@ $(document).ready(function(){
         // 如果 span 元素是可見的，則隱藏它，否則顯示它
         $(listSpan).toggle();
 
-
         let listTextColor = $(this).css('color');
 
         // 要寫嚴格等於，且不能寫 #色碼
@@ -86,19 +77,43 @@ $(document).ready(function(){
         }
 
 
-        //     $(this).toggle(function(){
-        //         $(this).css('color','#333')
-        //     },function(){
-        //         $(this).css('color','#999')
-        //     })
+        // 對應條件商品卡顯示
+        // let categoryName = $(this).text();
 
-        // let listBtnBorder = $(this).css('border');
+        // $(".card_zone ul > li").each(function(){
+        //     let categoryCard = $(this).data('class');
+        //     if (categoryName != categoryCard){
+        //         $(this).toggle();
+        //         // $(this).show();
+        //         // $(`.card_zone ul > li`).not(this).hide();
 
-        // if(listBtnBorder != '1px solid rgb(153, 153, 153)'){
-        //     $(this).css('border','1px solid rgb(153, 153, 153)')
-        // }else{
-        //     $(this).css('border','transparent')
-        // }
+        //     } else {
+        //         // $(`.card_zone ul > li`).not(this).hide();
+        //         // $(this).toggle();
+        //     }
+        // });
+
+
+        // 切換按鈕的選中狀態
+        $(this).toggleClass('active');
+        let clickBtn = $(`.list_btn button.active`)
+
+        // 檢查是否所有按鈕都未選取
+        if ($(clickBtn).length === 0) {         // 確認類陣列裡的個數
+            $(".card_zone ul > li").show();     // 顯示所有商品卡
+            return;                             // 結束函數，避免後續代碼執行
+        }
+
+        $(".card_zone ul > li").each(function(){
+            let categoryCard = $(this).data('class');
+            
+            // 如果按鈕是選中狀態且與商品卡片對應的類別名稱匹配，則顯示商品卡片，否則隱藏
+            if ($(clickBtn).filter(":contains(" + categoryCard + ")").length > 0) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
 
     });
 
@@ -109,10 +124,12 @@ $(document).ready(function(){
 
         let clickedBtn = $(`.list_btn`).find('span')
         let btnColor = $(`.list_btn`).find('button')
+        let categoryCard = $(".card_zone ul > li")
 
         if(clickedBtn.is(':visible')){
             clickedBtn.show();
             btnColor.css('color', '#999');
+            categoryCard.show()
         }
 
     })
